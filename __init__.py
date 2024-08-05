0cdd7a391c942288ca3ace4b4a0d776fb641d582
Shay Hill <shay_public@hotmail.com>
2024-08-05 02:12:26 -0500

    build: setuptools and Python 3.12
    Breaks pytest-lazy-fixture, so package is broken at this point.

--- __init__.py
+++
@@ -1,6 +0,0 @@
-"""Allow modules to be imported from top-level."""
-
-from .halfedge.half_edge_elements import *
-from .halfedge.half_edge_constructors import *
-from .halfedge.validations import *
-from .halfedge.half_edge_object import HalfEdges
